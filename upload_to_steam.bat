1e5b2e0d8dbf5c5095a53ae2a99a89426581ee78
John Punch <15096106+cakama3a@users.noreply.github.com>
2026-06-17 16:02:31 +0300

    Bat

--- upload_to_steam.bat
+++ upload_to_steam.bat
@@ -1,19 +1,19 @@
 @echo off
-chcp 65001 > nul
+chcp 1251 > nul
 setlocal enabledelayedexpansion
 
 echo ===================================================
-echo   Оновлення та завантаження локалізації в Steam
+echo   Onovlennya ta zavantazhennya lokalizacii v Steam
 echo ===================================================
 echo.
 
-:: 1. Копіюємо оновлені файли локалізації в папку SteamMods гри Gloomhaven
+:: 1. Copy files
 set "SRC_DIR=C:\Users\cakam\Documents\GitHub\GloomHavenUa"
 set "DEST_DIR=C:\Users\cakam\AppData\LocalLow\FlamingFowlStudios\Gloomhaven\SteamMods\UkraineHaven"
 
-echo [1/3] Копіювання файлів локалізації...
-echo Джерело: %SRC_DIR%\LangPacks
-echo Ціль:    %DEST_DIR%\LangPacks
+echo [1/3] Kopiuvannya fajliv lokalizacii...
+echo Dzherelo: %SRC_DIR%\LangPacks
+echo Cil:    %DEST_DIR%\LangPacks
 echo.
 
 if not exist "%DEST_DIR%\LangPacks" mkdir "%DEST_DIR%\LangPacks"
@@ -23,19 +23,19 @@ copy /Y "%SRC_DIR%\thumbnail.png" "%DEST_DIR%\" > nul
 xcopy /E /I /Y "%SRC_DIR%\LangPacks" "%DEST_DIR%\LangPacks" > nul
 
 if %ERRORLEVEL% equ 0 (
-    echo [OK] Файли успішно скопійовано!
+    echo [OK] Fajly uspisno skopiyovano!
 ) else (
-    echo [ERROR] Помилка копіювання файлів.
+    echo [ERROR] Pomylka kopiuvannya fajliv.
     pause
     exit /b %ERRORLEVEL%
 )
 echo.
 
-:: 2. Запитуємо параметри облікового запису та оновлення
-echo [2/3] Введіть дані для входу в Steam:
-set /p STEAM_USER="Введіть логін Steam: "
+:: 2. Credentials
+echo [2/3] Vvedit login Steam:
+set /p STEAM_USER="Login: "
 
-:: Створюємо тимчасовий VDF файл конфігурації для SteamCMD
+:: VDF File
 set "VDF_FILE=%SRC_DIR%\steamcmd\workshop_upload.vdf"
 (
 echo "workshopitem"
@@ -48,17 +48,16 @@ echo }
 ) > "%VDF_FILE%"
 
 echo.
-echo [3/3] Запуск завантаження в Steam Workshop...
-echo (Вам потрібно буде ввести ваш пароль Steam і, якщо увімкнено, код Steam Guard)
+echo [3/3] Zapusk zavantazhennya v Steam Workshop...
 echo.
 
 "%SRC_DIR%\steamcmd\steamcmd.exe" +login "%STEAM_USER%" +workshop_build_item "%VDF_FILE%" +quit
 
-:: Видаляємо тимчасовий VDF файл задля чистоти
+:: Delete temp VDF
 if exist "%VDF_FILE%" del "%VDF_FILE%"
 
 echo.
 echo ===================================================
-echo Процес завершено.
+echo Process zaversheno.
 echo ===================================================
 pause
